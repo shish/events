dd86fd4c5fccfe3e1bf953c81e855445e71a8440
Shish <shish@shishnet.org>
2024-06-02 21:30:13 +0100

    test

--- tests/data.sql
+++ tests/data.sql
@@ -1,3 +1,2 @@
-INSERT INTO users VALUES(1,'test','098f6bcd4621d373cade4e832627b4f6','','testing <b>boldly</b>');
-INSERT INTO users VALUES(2,'test2','ad0234829205b9033196ba818f7a872b','','');
-INSERT INTO users VALUES(3,'noavs','6ce85dd6c242477580ca2a210e75bfd7','','');
+INSERT INTO user VALUES(1,'admin','','$2b$12$mvkupOBN4BsZNfdoR1MKDOuBP0iNcrBzj7/7Vv75mUdCWToPq0j06','admin@example.com','2024-04-29 14:44:08.925836','2024-04-29 14:44:08.925839');
+
